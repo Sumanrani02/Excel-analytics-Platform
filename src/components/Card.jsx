84437e7fbed27fb3561a283090c5dba2ef190ebd
itzayush7 <mishraayush986@gmail.com>
2025-05-14 18:32:49 +0530

    Delete src directory

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Card = ({ children, className }) => {
-  return (
-    <div className={`flex h-screen w-screen items-center justify-center bg-green-50 ${className}`}>
-      {children}
-    </div>
-  );
-};
